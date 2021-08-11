d705a2a76b8e3bd297b9616510402fa7ca4197fd
Ben Jameson <benjamesonn@gmail.com>
2021-08-11 21:53:25 +1000

    drag and drop uploader done

--- index.js
+++ index.js
@@ -124,3 +124,77 @@ function updateAudioElements(accompanimentURL, vocalsURL) {
     document.getElementById("accompaniment").src = accompanimentURL;
     document.getElementById("vocals").src = vocalsURL;
 }
+
+
+
+
+
+
+/* Events fired on the drag target */
+
+// document.addEventListener("dragstart", function (event) {
+//     console.log("dragstart");
+//     // The dataTransfer.setData() method sets the data type and the value of the dragged data
+//     event.dataTransfer.setData("Text", event.target.id);
+
+//     // Output some text when starting to drag the p element
+//     document.getElementById("demo").innerHTML = "Started to drag the p element.";
+
+//     // Change the opacity of the draggable element
+//     event.target.style.opacity = "0.4";
+// });
+
+// While dragging the p element, change the color of the output text
+document.addEventListener("drag", function (event) {
+    // console.log("drag");
+    document.getElementById("demo").style.color = "red";
+});
+
+// Output some text when finished dragging the p element and reset the opacity
+document.addEventListener("dragend", function (event) {
+    document.getElementById("demo").innerHTML = "Finished dragging the p element.";
+    event.target.style.opacity = "1";
+});
+
+/* Events fired on the drop target */
+
+// When the draggable p element enters the droptarget, change the DIVS's border style
+document.addEventListener("dragenter", function (event) {
+    console.log("dragenter");
+    console.log(event.target);
+    if (event.target.className == "droptarget") {
+        document.getElementById("droptarget").style.background = "rgba(217, 217, 217, 0.75)";
+        document.getElementById("insideDropTarget").style.zIndex = "-1";
+        console.log("drop target");
+    }
+});
+
+// By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element
+document.addEventListener("dragover", function (event) {
+    // console.log("dragover");
+    event.preventDefault();
+});
+
+// When the draggable p element leaves the droptarget, reset the DIVS's border style
+document.addEventListener("dragleave", function (event) {
+    if (event.target.className == "droptarget") {
+        document.getElementById("droptarget").style.background = "";
+        document.getElementById("insideDropTarget").style.zIndex = "";
+    }
+});
+
+/* On drop - Prevent the browser default handling of the data (default is open as link on drop)
+   Reset the color of the output text and DIV's border color
+   Get the dragged data with the dataTransfer.getData() method
+   The dragged data is the id of the dragged element ("drag1")
+   Append the dragged element into the drop element
+*/
+document.addEventListener("drop", function (event) {
+    event.preventDefault();
+    console.log("DROPPPPPED");
+    var data = event.dataTransfer.items[0].getAsFile();
+    console.log(data);
+    if (event.target.className == "droptarget") {
+        uploadAndRunAI(data);
+    }
+});
