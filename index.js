5c7ba125640e29683e993264d9b5ca6ad6e8cc54
Ben Jameson <benjamesonn@gmail.com>
2021-09-16 17:48:27 +1000

    estimated times dictionary

--- index.js
+++ index.js
@@ -423,7 +423,7 @@ async function slowMessage() {
 
 async function timeoutErrorMessage() {
     console.log('timeout error timer started');
-    await new Promise(r => setTimeout(r, 600000)); //10 minutes
+    await new Promise(r => setTimeout(r, 1200000)); //20 minutes
     if (document.getElementById('resultsDiv').classList.contains('hideElement')) {
         view_error();
     }
@@ -579,3 +579,26 @@ function closeNav() {
 }
 
 
+let estimated_times = {
+    //first number rounds nearest minutes, second is 5/4 tracks or 2 tracks
+    '0_2': { 'message': 'Estimated time 29 seconds', 'milliseconds': 29000 },
+    '0_5': { 'message': 'Estimated time 29 seconds', 'milliseconds': 29000 },
+    '1_2': { 'message': 'Estimated time 29 seconds', 'milliseconds': 29000 },
+    '1_5': { 'message': 'Estimated time 29 seconds', 'milliseconds': 29000},
+    '2_2': { 'message': 'Estimated time 29 seconds', 'milliseconds': 29000 },
+    '2_5': { 'message': 'Estimated time 39 seconds', 'milliseconds': 39000 },
+    '3_2': { 'message': 'Estimated time 29 seconds', 'milliseconds': 29000 },
+    '3_5': { 'message': 'Estimated time 59 seconds', 'milliseconds': 59000 },
+    '4_2': { 'message': 'Estimated time 39 seconds', 'milliseconds': 39000 },
+    '4_5': { 'message': 'Estimated time 1 minute 9 seconds', 'milliseconds': 69000 },
+    '5_2': { 'message': 'Estimated time 49 seconds', 'milliseconds': 49000 },
+    '5_5': { 'message': 'Estimated time 1 minute 39 seconds', 'milliseconds': 99000 },
+    '5-7_2': { 'message': 'Estimated time 49 seconds', 'milliseconds': 49000 },
+    '5-7_5': { 'message': 'Estimated time 1 minute 49 seconds', 'milliseconds': 109000 },
+    '7-14_2': { 'message': 'Estimated time 2 minutes 10 seconds', 'milliseconds': 109000 },
+    '7-14_5': { 'message': 'Your audio file is longer than usual, please allow up to 5 minutes', 'milliseconds': 109000 },
+    '14-21_2': { 'message': 'Estimated time 3 minutes 20 seconds', 'milliseconds': 109000 },
+    '14-21_5': { 'message': 'Your audio file is longer than usual, please allow up to 6 minutes', 'milliseconds': 360000 },
+    '21+_2': { 'message': 'Your audio file is longer than usual, please allow up to 5 minutes', 'milliseconds': 109000 },
+    '21+_5': { 'message': 'Your audio file is longer than usual, please allow up to 8 minutes', 'milliseconds': 480000 }
+}
